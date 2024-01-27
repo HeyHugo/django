5987514da121d35aa89324199dbb197ad15c8328
Hugo Heyman <hugo.heyman@gmail.com>
2024-01-27 16:22:22 +0100

    Add debug print for http.disconnect

--- django/core/handlers/asgi.py
+++ django/core/handlers/asgi.py
@@ -227,6 +227,7 @@ class ASGIHandler(base.BaseHandler):
         """Listen for disconnect from the client."""
         message = await receive()
         if message["type"] == "http.disconnect":
+            print("http.disconnect")
             raise RequestAborted()
         # This should never happen.
         assert False, "Invalid ASGI message after request body: %s" % message["type"]
